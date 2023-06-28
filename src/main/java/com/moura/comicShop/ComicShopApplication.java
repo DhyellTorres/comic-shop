450e9bb1662a5b14c9d0728bc72c0f7b825bd950
DhyellTorres <torresadryel@gmail.com>
2023-06-28 10:14:19 -0300

    inicio projeto

---
+++ src/main/java/com/moura/comicShop/ComicShopApplication.java
@@ -0,0 +1,13 @@
+package com.moura.comicShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moura/comicShop/ComicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moura.comicShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
